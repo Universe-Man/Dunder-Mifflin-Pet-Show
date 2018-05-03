23d920f0ced099b327acff5ac9e4a5a2e25cdf37
github username <ianjpollack@gmail.com>
2018-05-03 17:19:22 -0400

    ewe did it

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,40 @@
 class DogsController < ApplicationController
+  before_action :find_dog, only: [:show, :edit, :update]
+  def index
+    @dogs = sorted_dogs
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def show
+    # before_action
+  end
+
+  def edit
+    # before_action
+
+  end
+
+  def create
+    # before_action
+
+  end
+  def update
+
+  end
+
+  private
+  def sorted_dogs
+    Dog.all.sort_by do |dog|
+     dog.employees.size
+   end.reverse
+ end
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
+  def find_dog
+    @dog = Dog.find(params[:id])
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+  validates :alias, :title, uniqueness: true
+  # validates :alias, uniqueness: {scope: :title}
+  # validates :title, uniqueness: {scope: :alias}
+
 end
